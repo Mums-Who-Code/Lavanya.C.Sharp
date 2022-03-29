b826c545599d574756b223bac94b64f39e217851
Lavanya Juturu <jlavanyareddy@gmail.com>
2022-03-28 18:51:03 -0700

    Fixed requested changes

--- Lavanya.C.Sharp/Set1Task4/Program.cs
+++ Lavanya.C.Sharp/Set1Task4/Program.cs
@@ -3,7 +3,7 @@ int a = 15;
 int b = 10;
 decimal c;
 
-c = a + b;
+c = a+b;
 Console.WriteLine($"Sum of {a} and {b} is {c}");
 Print(++a, --b);
 
@@ -20,7 +20,7 @@ Console.WriteLine($"quotient of {a} and {b} is {c}");
 Print(++a, --b);
 
 c = a%b;
-Console.WriteLine($"Reaminder of {a} and {b} is {c}");
+Console.WriteLine($"Remainder of {a} and {b} is {c}");
 Print(++a, --b);
 
 void Print(int a , int b)
