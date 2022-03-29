f6224bbafa7a859681741f57a9536a11ff16b4be
Lavanya Juturu <jlavanyareddy@gmail.com>
2022-03-28 18:44:27 -0700

    Set1Task4

---
+++ Lavanya.C.Sharp/Set1Task4/Program.cs
@@ -0,0 +1,30 @@
+﻿
+int a = 15;
+int b = 10;
+decimal c;
+
+c = a + b;
+Console.WriteLine($"Sum of {a} and {b} is {c}");
+Print(++a, --b);
+
+c = a-b;
+Console.WriteLine($"Difference of {a} and {b} is {c}");
+Print(++a, --b);
+
+c = a*b;
+Console.WriteLine($"Product of {a} and {b} is {c}");
+Print(++a, --b);
+
+c = a/b;
+Console.WriteLine($"quotient of {a} and {b} is {c}");
+Print(++a, --b);
+
+c = a%b;
+Console.WriteLine($"Reaminder of {a} and {b} is {c}");
+Print(++a, --b);
+
+void Print(int a , int b)
+{
+    Console.WriteLine("a is " + a);
+    Console.WriteLine("b is " + b);
+}
