b9921ec8f3555373f11e35dfa810c95f2a3a6ec6
Lavanya Juturu <jlavanyareddy@gmail.com>
2022-04-02 11:37:39 -0700

    Fixed review comments

--- Lavanya.C.Sharp/Set1Task5/Program.cs
+++ Lavanya.C.Sharp/Set1Task5/Program.cs
@@ -5,5 +5,3 @@ Random random = new Random();
 int randomNumber = random.Next(1,100);
 DateTime newDateTime = currentDateTime.AddDays(randomNumber);
 Console.WriteLine($"Updated date and time is {newDateTime} after adding {randomNumber} days to {currentDateTime}");
-
-
