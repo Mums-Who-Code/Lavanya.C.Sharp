f5f4c7b39b212eab397214662d9115a2b3df78ab
Lavanya Juturu <jlavanyareddy@gmail.com>
2022-04-06 21:43:44 -0700

    Set1Task6 and Set1Task7

---
+++ Lavanya.C.Sharp/Set1Task6/Program.cs
@@ -0,0 +1,65 @@
+﻿
+Random random = new Random();
+int [] players = new int [5];
+for (int i = 0; i < 5; i++) 
+{
+    players[i] = random.Next(1, 7);
+}
+
+if (ContainsDuplicates(players, players[0]))
+{
+    Console.WriteLine($"First Player rolled {players[0]} and moved to the next level");
+}
+else 
+{
+    Console.WriteLine($"First Player rolled {players[0]} and did not move to the next level");
+}
+if (ContainsDuplicates(players, players[1]))
+{
+    Console.WriteLine($"Second Player rolled {players[1]} and moved to the next level");
+}
+else 
+{
+    Console.WriteLine($"Second Player rolled {players[1]} and did not move to the next level");
+}
+if (ContainsDuplicates(players, players[2]))
+{
+    Console.WriteLine($"Third Player rolled {players[2]} and moved to the next level");
+}
+else 
+{
+    Console.WriteLine($"Third Player rolled {players[2]} and did not move to the next level");
+}
+if (ContainsDuplicates(players, players[3]))
+{
+    Console.WriteLine($"Fourth Player rolled {players[3]} and moved to the next level");
+}
+else
+{
+    Console.WriteLine($"Fourth Player rolled {players[3]} did not move to the next level");
+}
+if (ContainsDuplicates(players, players[4]))
+{
+    Console.WriteLine($"Fifth Player rolled {players[4]} and moved to the next level");
+}
+else 
+{
+    Console.WriteLine($"Fifth Player rolled {players[4]} and did not move to the next level");
+}
+
+static bool ContainsDuplicates(int[] playerList,int number)
+{
+    int count = 0;
+    for(int i=0;i< playerList.Length;i++)
+    {
+        if (playerList[i] == number)
+        {
+            count++;
+        }
+    }
+    if (count > 1)
+    {
+        return true;
+    }
+    return false;
+}
---
+++ Lavanya.C.Sharp/Set1Task7/Program.cs
@@ -0,0 +1,49 @@
+﻿
+using System.Collections.Generic;
+
+List<int> playerOne = new List<int> { 1, 5, 10, 25, 50 };
+List<int> playerTwo = new List<int> { 1, 5, 10, 25, 50 };
+Random random = new Random();
+
+double playerOneTotal = 0;
+double playerTwoTotal = 0;
+
+for (int i = playerOne.Count; i >0; i--)
+{ 
+    int playerOneIndex = random.Next(i);
+    int playerOneCoin = playerOne[playerOneIndex];
+    int playerTwoIndex = random.Next(i);
+    int playerTwoCoin = playerTwo[playerTwoIndex];
+    playerOne.RemoveAt(playerOneIndex);
+    playerTwo.RemoveAt(playerTwoIndex);
+
+    Console.WriteLine($"PlayerOne presents coin {playerOneCoin}.");
+    Console.WriteLine($"PlayerTwo presents coin {playerTwoCoin}.");
+
+    if (playerOneCoin > playerTwoCoin)
+    {
+        playerOneTotal += 1;
+    }
+    else if (playerTwoCoin > playerOneCoin)
+    {
+        playerTwoTotal += 1;
+    }
+    else 
+    {
+        playerOneTotal += 0.5;
+        playerTwoTotal += 0.5;
+    }
+}
+
+if (playerOneTotal > playerTwoTotal)
+{
+    Console.WriteLine($"PlayerOne won with {playerOneTotal} points.");
+}
+else if (playerTwoTotal > playerOneTotal)
+{
+    Console.WriteLine($"PlayerTwo won with {playerTwoTotal} points.");
+}
+else
+{
+    Console.WriteLine($"Game is tied with {playerOneTotal} points for both PlayerOne and playerTwo");
+}
